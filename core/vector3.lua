348642edd0067f19daedbca5f5f571127486ff0f
ANormalTwig <1andonlytwig@gmail.com>
2023-05-29 13:11:19 -0400

    More Vector3 methods

--- core/vector3.lua
+++ core/vector3.lua
@@ -37,16 +37,51 @@ function Vector3:set(x, y, z)
 	self.z = z
 end
 
+--- Returns the dot product of two vectors (cosine of the angle between two vectors multiplied their length).
+---@return number dot Dot product.
+function Vector3:dot(v)
+	return self.x * v.x + self.y * v.y + self.z * v.z
+end
+
 --- Gets the squared distance between two vectors
 ---@param v Vector3
+---@return number distance Distance squared.
 function Vector3:getDistanceSqr(v)
 	return (v.x - self.x)^2 + (v.y - self.y)^2 + (v.z - self.z)^2
 end
 
 --- Gets the distance between two vectors
 ---@param v Vector3
+---@return number distance Distance.
 function Vector3:getDistance(v)
-	return math_sqrt(self:getDistanceSqr(v))
+	return math_sqrt((v.x - self.x)^2 + (v.y - self.y)^2 + (v.z - self.z)^2)
+end
+
+--- Returns the length of the vector squared.
+---@return number length Squared length
+function Vector3:getLengthSqr()
+	return self.x^2 + self.y^2 + self.z^2
+end
+
+--- Returns the length of the vector.
+---@return number length Length.
+function Vector3:getLength()
+	return math_sqrt(self.x^2 + self.y^2 + self.z^2)
+end
+
+--- Normalizes a vector
+function Vector3:normalize()
+	local n = math_sqrt(self.x^2 + self.y^2 + self.z^2)
+	self.x = self.x / n
+	self.y = self.y / n
+	self.z = self.z / n
+end
+
+--- Returns the vector normalized
+---@return Vector3 normal
+function Vector3:getNormalized()
+	local n = math_sqrt(self.x^2 + self.y^2 + self.z^2)
+	return Vector3:new(self.x / n, self.y / n, self.z / n)
 end
 
 return Vector3
