f5fa48fb7b2f2d58251769ace10e0eda803b2de3
ANormalTwig <1andonlytwig@gmail.com>
2023-07-04 23:12:20 -0400

    Pool minimum value

--- core/pool.lua
+++ core/pool.lua
@@ -1,31 +1,36 @@
+local math_max = math.max
+
 ---@class Pool
----@field max number Max amount of IDs the pool can manage.
+---@field count number Number of IDs the pool can manage.
+---@field min number Lowest value the pool will return.
 ---@field used table<number, true|nil>
 ---@field index number Position the Pool will check before trying the next ID.
 local Pool = {}
 Pool.__index = Pool
 
 --- Create a new Pool.
----@param max number
+---@param count number
+---@param min number? the minimum value to be returned
 ---@return Pool
-function Pool:new(max)
+function Pool:new(count, min)
 	return setmetatable({
 		used = {},
-		max = max,
-		index = 1,
+		count = count,
+		min = min or 1,
+		index = min or 1,
 	}, self)
 end
 
 --- Get an ID from the pool
+---@return number|nil id Returns the id or nil if there are none left.
 function Pool:getID()
-	for _ = 1, self.max do
+	for _ = 1, self.count do
 		if not self.used[self.index] then
 			self.used[self.index] = true
 			return self.index
 		end
-		self.index = (self.index % self.max) + 1
+		self.index = math_max(self.index % (self.min + self.count - 1), self.min - 1) + 1
 	end
-	return false
 end
 
 --- Free an ID to the pool
